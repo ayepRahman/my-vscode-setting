8310063f71f3bc44c5184aead8e95542dc4d3034
arifrahman <matrep88@gmail.com>
2019-07-23 15:30:02 +0800

    initial setting

---
+++ setting.js
@@ -0,0 +1,66 @@
+{
+  "window.zoomLevel": 0,
+  "workbench.iconTheme": "material-icon-theme",
+  "workbench.sideBar.location": "left",
+  "emmet.showSuggestionsAsSnippets": true,
+  "emmet.triggerExpansionOnTab": true,
+  "emmet.syntaxProfiles": {
+    "javascript": "jsx"
+  },
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact",
+    "vue-html": "html",
+    "plaintext": "jade"
+  },
+  "material-icon-theme.showUpdateMessage": false,
+  "files.autoSave": "onFocusChange",
+  "files.autoSaveDelay": 3000,
+  "editor.fontSize": 12,
+  "editor.foldingStrategy": "indentation",
+  "editor.fontFamily": "Operator Mono",
+  // "editor.fontFamily": "Fira Code,Operator Mono",
+  "editor.fontLigatures": true,
+  "editor.fontWeight": "300",
+  "editor.lineHeight": 18,
+  "editor.suggestSelection": "first",
+  "editor.formatOnSave": true,
+  "editor.minimap.enabled": false,
+  "editor.snippetSuggestions": "top",
+  "editor.detectIndentation": false,
+  "editor.tabSize": 2,
+  "editor.renderWhitespace": "all",
+  // prettier
+  "[javascript]": {
+    "editor.formatOnSave": true
+    // "editor.defaultFormatter": "vscode.typescript-language-features"
+  },
+  "[css]": {
+    "editor.formatOnSave": true
+  },
+  "[scss]": {
+    "editor.formatOnSave": true
+  },
+  "[handlebars]": {
+    "editor.formatOnSave": false
+  },
+  "eslint.autoFixOnSave": true,
+  "eslint.alwaysShowStatus": true,
+  "eslint.packageManager": "yarn",
+  "prettier.arrowParens": "avoid",
+  "prettier.printWidth": 100,
+  "prettier.singleQuote": true,
+  "prettier.trailingComma": "all",
+  "prettier.tabWidth": 2,
+  "prettier.semi": true,
+  "prettier.bracketSpacing": true,
+  "path-intellisense.autoSlashAfterDirectory": true,
+  "workbench.activityBar.visible": true,
+  "workbench.statusBar.visible": true,
+  "workbench.colorTheme": "Material Theme Darker High Contrast",
+  "diffEditor.ignoreTrimWhitespace": false,
+  "git.confirmSync": false,
+  "git.ignoreLimitWarning": true,
+  "javascript.updateImportsOnFileMove.enabled": "never",
+  "explorer.confirmDragAndDrop": false,
+  "vsintellicode.modify.editor.suggestSelection": "automaticallyOverrodeDefaultValue"
+}
